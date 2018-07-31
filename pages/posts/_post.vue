d94626d2ea9af371610e29eab064072f3ae8e40f
George Bennett <george6bennett@gmail.com>
2018-07-31 08:38:20 +0100

    dynamic meta

--- pages/posts/_post.vue
+++ pages/posts/_post.vue
@@ -9,14 +9,12 @@
 export default {
   async asyncData ({params}) {
     const body = await import(`~/static/content/${params.post}.md`)
-    const meta = await import(`~/static/meta/${params.post}`)
-    const metaaa = await import(`~/static/meta/all`)
-    const index = metaaa.default.findIndex(p => p.permalink == params.post)
-    console.log(index)
-    console.log(metaaa.default[index])
+    const allMeta = await import(`~/static/meta/all`)
+    const index = allMeta.default.findIndex(p => p.permalink == params.post)
+    const meta = allMeta.default[index]
     return {
       body: body,
-      meta: meta.default
+      meta: meta
     }
   }
 }
--- static/meta/strawberry-frangipane.js
+++
@@ -1,9 +0,0 @@
-const meta = {
-  permalink: "strawberry-frangipane",
-  title: "Strawberry Frangipane Step-By-Step",
-  description: "This strawberry frangipane is shop bought and really nice in the microwave. Nice lemon finish.",
-  date: "2018-07-17",
-  tags: ["sweet", "mushroom" ,"hello"]
-}
-
-export default meta
