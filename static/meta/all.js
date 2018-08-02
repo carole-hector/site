a998003fc196d087600c799cbf2e9023ae5af2a2
carolehector93 <42046248+carolehector93@users.noreply.github.com>
2018-08-02 21:10:54 +0100

    Update all.js

--- static/meta/all.js
+++ static/meta/all.js
@@ -1,9 +1,15 @@
 const meta = [{
+  permalink: "potato-salad",
+  title: "Trini Potato Salad",
+  description: "In Trinidad, Potato salad is a classic side dish for nearly any Trini meal, and it’s an essential part of the Sunday table too.",
+  date: "2018-07-29",
+  tags: ["trinidad", "potato" ,"mayonnaise", "savoury"]
+},{
   permalink: "pepper-shrimp",
   title: "Trini Pepper Shrimp",
   description: "Pepper shrimp is the love child of Trinidad’s Chinese ancestry and traditional Trinidadian flavours.",
   date: "2018-07-29",
-  tags: ["pepper", "shrimp" ,"trinidad"]
+  tags: ["pepper", "shrimp" ,"trinidad", "savoury"]
 }]
 
 export default meta
