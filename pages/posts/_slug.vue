a0690cbcf84165bf1f54714c2211b92ab0c2b1f9
geeogi <35661160+geeogi@users.noreply.github.com>
2019-02-04 20:52:52 +0000

    add sub ingredient feature (#28)

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,34 +1,72 @@
-<template lang="pug">
-  div.container
-    div.columns.is-marginless
-      // Desktop
-      div.column.is-4.is-hidden-mobile
-        div.card
-          div.card-image
-            figure.is-square
-              v-image(:src="img")
-          div.card-content
-            div.content
-              h4 Ingredients
-              ul
-                li(v-for="ingredient in copy.ingredients" :key="ingredient" v-text="ingredient")
-      div.column.is-paddingless
-        // Mobile
-        figure.is-square.is-hidden-tablet
-          v-image(:src="img")
-        // Both
-        div.card-content
-          div.content
-            h2.title(v-text="copy.title")
-            h6(v-text="subtitle")
-            p(v-text="copy.description")
-            // Mobile
-            h4.is-hidden-tablet Ingredients
-            ul.is-hidden-tablet
-              li(v-for="ingredient in copy.ingredients" :key="ingredient" v-text="ingredient")
-            h4 Method
-            ol
-              li(v-for="step in copy.method" :key="step" v-text="step")
+<template>
+  <div class="container">
+    <div class="columns is-marginless">
+      <!-- Desktop -->
+      <div class="column is-4 is-hidden-mobile">
+        <div class="card">
+          <div class="card-image">
+            <figure class="is-square">
+              <v-image :src="img"/>
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="content">
+              <h3>Ingredients</h3>
+              <template v-for="(subsection, index) in copy.ingredients">
+                <p
+                  :key="index"
+                  v-if="subsection.name"
+                  class="is-italic"
+                >For the {{subsection.name}}:</p>
+                <ul :key="index">
+                  <li
+                    v-for="ingredient in subsection.ingredients"
+                    :key="ingredient"
+                    v-text="ingredient"
+                  />
+                </ul>
+              </template>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column is-paddingless">
+        <!-- Mobile -->
+        <figure class="is-square is-hidden-tablet">
+          <v-image :src="img"/>
+        </figure>
+        <!-- All -->
+        <div class="card-content">
+          <div class="content">
+            <h2 class="title">{{copy.title}}</h2>
+            <h6>{{subtitle}}</h6>
+            <p v-text="copy.description"/>
+            <!-- Mobile -->
+            <h3 class="is-hidden-tablet">Ingredients</h3>
+            <template v-for="(subsection, index) in copy.ingredients">
+              <p
+                :key="index"
+                v-if="subsection.name"
+                class="is-italic is-hidden-tablet"
+              >For the {{subsection.name}}:</p>
+              <ul :key="index" class="is-hidden-tablet">
+                <li
+                  v-for="ingredient in subsection.ingredients"
+                  :key="ingredient"
+                  v-text="ingredient"
+                />
+              </ul>
+            </template>
+            <!-- All -->
+            <h3>Method</h3>
+            <ol>
+              <li v-for="step in copy.method" :key="step" v-text="step"/>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
