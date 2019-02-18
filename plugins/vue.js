f1bb3b82cd186ed41ce9832551e926b87e5e1f29
George Bennett <george6bennett@gmail.com>
2019-02-18 20:46:13 +0000

    refactor: use uppercase for imports

--- plugins/vue.js
+++ plugins/vue.js
@@ -1,12 +1,12 @@
 import Vue from "vue"
-import Vdate from "~/components/v-date.vue"
-import VpostPreview from "~/components/v-post-preview.vue"
-import VviewsCounter from "~/components/v-views-counter.vue"
-import Vimage from "~/components/v-image.vue"
+import VDate from "~/components/v-date.vue"
+import VPostPreview from "~/components/v-post-preview.vue"
+import VViewsCounter from "~/components/v-views-counter.vue"
+import VImage from "~/components/v-image.vue"
 import VIcon from "~/components/v-icon.vue"
 
-Vue.component(Vdate.name, Vdate)
-Vue.component(VpostPreview.name, VpostPreview)
-Vue.component(VviewsCounter.name, VviewsCounter)
-Vue.component(Vimage.name, Vimage)
+Vue.component(VDate.name, VDate)
+Vue.component(VPostPreview.name, VPostPreview)
+Vue.component(VViewsCounter.name, VViewsCounter)
+Vue.component(VImage.name, VImage)
 Vue.component(VIcon.name, VIcon)
