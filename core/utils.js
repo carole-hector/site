bd882f5539d74133ec26d73028fe054dd7b71424
George Bennett <george6bennett@gmail.com>
2019-02-24 12:07:43 +0000

    delete: unused core utils

--- core/utils.js
+++ core/utils.js
@@ -1,14 +1,1 @@
-export function matches(post, v) {
-  const l = v.toLowerCase()
-  return (
-    post.tags.includes(l) ||
-    post.title.toLowerCase().match(l) ||
-    post.description.toLowerCase().match(l)
-  )
-}
-
-export function filterPosts(posts, v) {
-  return v ? posts.filter(p => matches(p, v)) : posts
-}
-
 export const stringify = obj => JSON.stringify(obj, null, 2)
