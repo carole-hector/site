99de33b8fa9850e7e9f190f74eb58c7348165d9c
George Bennett <george6bennett@gmail.com>
2019-02-24 12:08:09 +0000

    update: routes generation structure

--- nuxt/routes.js
+++ nuxt/routes.js
@@ -1,5 +1,9 @@
 import { SLUGS } from "../content/posts.json"
 
-const POST_ROUTES = SLUGS.map(slug => `posts/${slug}`)
-const SEARCH_ROUTES = ["search/sweet", "search/savoury"]
+export const POST_PATHS = SLUGS
+export const SEARCH_PATHS = ["sweet", "savoury"]
+
+const POST_ROUTES = POST_PATHS.map(slug => `posts/${slug}`)
+const SEARCH_ROUTES = SEARCH_PATHS.map(path => `/search/${path}`)
+
 export const routes = [...SEARCH_ROUTES, ...POST_ROUTES]
